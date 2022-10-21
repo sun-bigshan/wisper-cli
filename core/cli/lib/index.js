40527d5ca049d3789a91f06bad333039e94ec48c
sun-bigshan <>
2022-10-21 13:46:13 +0800

    feat: update core

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,28 +1,95 @@
 'use strict';
 
-module.exports = core;
-
+const path = require('path');
 const semver = require('semver');
 const colors = require('colors');
+const userHome = require('user-home');
+const pathExists = require('path-exists').sync;
 
 const pkg = require('../package.json');
 const log = require('@wisper-cli/log');
 const constant = require('./const');
 
-function core() {
+let args, config;
+
+async function core() {
   try {
     checkPkgVersion();
     checkNodeVersion();
     checkRoot();
+    checkUserHome();
+    checkInputArgs();
+    log.verbose('debug', 'test debug log');
+    checkEnv();
+    checkGlobalUpdate();
   } catch (err) {
     log.error(err.message);
+  }
+}
+
+async function checkGlobalUpdate() {
+  // 1. 获取当前的版本号和模块名
+  const currentVersion = pkg.version;
+  const npmName = pkg.name;
+  // 2. 调用 npm API 获取所有版本号
+  const { getNpmSemverVersion } = require('@wisper-cli/get-npm-info');
+  // 3. 提取所有版本号，比对哪些版本号时大于当前版本号
+  const lastVersion = await getNpmSemverVersion(npmName, currentVersion);
+  // 4. 获取最新的版本号，提示用户更新到该版本
+  if (lastVersion && semver.gt(lastVersion, currentVersion)) {
+    log.warn(
+      colors.yellow(
+        `请手动更新 ${npmName}, 当前版本为：${currentVersion}, 最新版本为：${lastVersion}
+              更新命令为：npm install -g ${npmName}`
+      )
+    );
+  }
+}
+
+function checkEnv() {
+  const dotenv = require('dotenv');
+  const dotenvPath = path.resolve(userHome, '.env');
+  if (pathExists(dotenvPath)) {
+    config = dotenv.config({
+      path: path.resolve(userHome, '.env'),
+    });
+  }
+  createDefaultConfig();
+  log.verbose('环境变量', process.env.CLI_HOME_PATH);
+}
+
+function createDefaultConfig() {
+  let cliHome;
+  if (process.env.CLI_HOME) {
+    cliHome = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliHome = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.CLI_HOME_PATH = cliHome;
+}
+
+function checkInputArgs() {
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  // console.log(args);
+  checkArgs();
+}
+
+function checkArgs() {
+  const level = args.debug ? 'verbose' : 'info';
+  process.env.LOG_LEVEL = level;
+  log.level = level;
+}
+
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('当前用户主目录不存在'));
   }
 }
 
 function checkRoot() {
   const rootCheck = require('root-check');
   rootCheck();
-  console.log(process.geteuid());
 }
 
 function checkNodeVersion() {
@@ -38,3 +105,5 @@ function checkNodeVersion() {
 function checkPkgVersion() {
   log.info('cli', pkg.version);
 }
+
+module.exports = core;
---
+++ utils/get-npm-info/lib/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const axios = require('axios');
+const urlJoin = require('url-join');
+const semver = require('semver');
+
+function getNpmInfo(npmName, registry) {
+  if (!npmName) {
+    return null;
+  }
+  const registryUrl = registry || getDefaultRegistry(true);
+  const npmInfoUrl = urlJoin(registryUrl, npmName);
+  return axios
+    .get(npmInfoUrl)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.data;
+      }
+      return null;
+    })
+    .catch((err) => {
+      // console.log('获取 npm 包信息失败', err.message);
+    });
+}
+
+function getDefaultRegistry(isOriginal = false) {
+  return isOriginal
+    ? 'https://registry.npmjs.org'
+    : 'https://registry.npm.taobao.org';
+}
+
+async function getNpmVersions(npmName, registry) {
+  const data = await getNpmInfo(npmName, registry);
+  if (data) {
+    return Object.keys(data.versions);
+  } else {
+    return [];
+  }
+}
+
+function getSemverVersions(baseVersion, versions = []) {
+  return versions
+    .filter((version) => semver.satisfies(version, `^${baseVersion}`))
+    .sort((a, b) => semver.gt(b, a));
+}
+
+async function getNpmSemverVersion(npmName, baseVersion, registry) {
+  const versions = await getNpmVersions(npmName, registry);
+  const newVersions = getSemverVersions(baseVersion, versions);
+  if (newVersions && newVersions.length > 0) {
+    return newVersions[0];
+  }
+}
+
+module.exports = {
+  getNpmInfo,
+  getNpmVersions,
+  getNpmSemverVersion,
+};
